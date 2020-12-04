29fb8d58df6ea554001b0819004dac1ce5d4715d
Andy <18270234907@163.com>
2020-12-04 09:51:20 +0800

    add zk service module

---
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -0,0 +1,24 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: Andy
+ * @Date: 2020/12/3 9:07
+ */
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @GetMapping("/payment/zk")
+    public String payment() {
+
+        return "springcloud with zookeeper: " + serverPort + "\t";
+    }
+}
